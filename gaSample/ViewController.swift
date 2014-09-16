89c3b949a3814f4c8e6615e031d84b7b7d898237
Takeshi Kawai <takezou621@gmail.com>
2014-09-16 16:52:00 +0900

    Initial Commit

---
+++ gaSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gaSample
+//
+//  Created by KawaiTakeshi on 2014/09/16.
+//  Copyright (c) 2014年 KawaiTakeshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
